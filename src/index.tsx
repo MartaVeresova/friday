9ba30e0b8f58cb2d84d9d35216c4b83a9e017061
MartaVeresova <MartaVeresovaa@gmail.com>
2021-07-21 15:21:01 +0300

    Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from './bll/store';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
         <HashRouter>
-            <App/>
+            <Provider store={store}>
+                <App/>
+            </Provider>
         </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
